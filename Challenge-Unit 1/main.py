4781f7041da9a5f29f453fc8bd64761f4c23698e
astvu20920922u09009 <144677728+astvu20920922u09009@users.noreply.github.com>
2023-09-11 16:36:14 +0530

    Add files via upload

---
+++ Challenge-Unit 1/main.py
@@ -0,0 +1,11 @@
+# implement a recusive function to calculate the factorial a given number
+
+
+def factorial(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+print(factorial(7))
